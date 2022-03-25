edc3963624fbae38103ee8bbb8a446879fe5aea9
asifsabir5020 <asif.sabir.5020@gmail.com>
2022-03-25 09:30:39 +0500

    mongo db and dotenv

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 const app = express();
+import dotEnv from 'dotenv';
+dotEnv.config();
+import { connectDB } from "./db/connection.js";
+
 
 
 
@@ -8,7 +12,19 @@ app.use('/', (req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log("Server running");
-});
 
+
+
+const start = async () => {
+    try{
+        const port = process.env.PORT || 8000;
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    }catch(error){
+        console.log(error);
+    }
+}
+
+start();
